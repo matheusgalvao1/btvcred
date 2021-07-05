e45eb0d0135dfa1d06d900055c632f60716de68d
Matheus Galvao <matheusrigonigalvao@gmail.com>
2021-07-05 08:48:26 -0300

    :art: Organizing main

---
+++ lib/view/home/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.all(30),
+        children: [
+          Text(
+            'Escolha uma opção',
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 30),
+          CupertinoButton.filled(
+            child: Text('Opção 1'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
