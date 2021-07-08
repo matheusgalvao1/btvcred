6c371f2e5b01047ec2e96efdd676699130a69b6d
Matheus Galvao <matheusrigonigalvao@gmail.com>
2021-07-08 13:44:48 -0300

    :construction: Router

--- lib/view/home/main.dart
+++ lib/view/home/main.dart
@@ -1,24 +1,22 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../../bloc/Simulator.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.all(30),
-        children: [
-          Text(
-            'Escolha uma opção',
-            textAlign: TextAlign.center,
+    return Consumer<BlocSimulator>(
+      builder: (BuildContext context, BlocSimulator bloc) {
+        return Scaffold(
+          body: PageView(
+            controller: bloc.pageController,
+            physics: NeverScrollableScrollPhysics(),
+            children: [],
           ),
-          SizedBox(height: 30),
-          CupertinoButton.filled(
-            child: Text(' 1'),
-            onPressed: () {},
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
